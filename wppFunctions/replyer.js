59c6773a95acff3846b0e0df090e56d83ff7e9b0
Jorge <jorgejapao00@gmail.com>
2026-07-26 20:58:50 -0300

    replyer class added

---
+++ wppFunctions/replyer.js
@@ -0,0 +1,31 @@
+import Groq from "groq-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+export class Replyer {
+    constructor(client) {
+        this.client = client;
+        this.groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+    }
+
+
+    async buildMessageStart(content, context) {
+        const answer = await this.groq.chat.completions.create({
+            messages: [
+                {
+                    role: "user",
+                    content: content + "A RESPOSTA DEVE SER SEMPRE EM PORTUGUÊS BRASILEIRO. Responda de forma concisa e direta, sem rodeios. Seja objetivo e claro em sua resposta. Não passe de 25 palavras. Fale de uma forma acolhedora, você está prestes a oferecer algum tipo de serviço relacionado a: " + context,
+                },
+            ],
+            model: "openai/gpt-oss-20b",
+        });
+
+        return answer.choices[0]?.message?.content || "";
+    }
+
+    async ping() {
+        const response = await this.buildMessageStart("Acabei de ser roubado..." + "Ofereça algum tipo de serviço relacionado a roubo, furto ou assalto, como um seguro.", "roubo");
+        return response + 'Você pode entrar em contato com nossa corretora de seguros para obter assistência imediata. Estamos aqui para ajudá-lo a lidar com essa situação e garantir que você receba o suporte necessário: ';
+    }
+
+}
