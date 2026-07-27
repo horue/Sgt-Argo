6f499338973cdb88b5048e1d843026348cb6c84b
Jorge <jorgejapao00@gmail.com>
2026-07-27 20:27:17 -0300

    entire bot moved to Bot class

---
+++ bot/bot.js
@@ -0,0 +1,60 @@
+import Replyer from '../wppFunctions/replyer.js';
+import Analyzer from '../wppFunctions/analyzer.js';
+import pkg from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
+const { Client, LocalAuth } = pkg;
+
+
+export default class Bot {
+    constructor() {
+        this.client = new Client({
+        authStrategy: new LocalAuth(),
+            puppeteer: {
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            }
+        });
+        this.qrcode = qrcode;
+        
+        this.replyer = new Replyer(this.client);
+        this.analyzer = new Analyzer(this.client);
+    }   
+
+    initialize() {
+        this.client.on('qr', (qr) => {
+            this.qrcode.generate(qr, { small: true });
+        });
+
+        this.  client.on('ready', () => {
+            console.log('Client is ready!');
+        });
+
+        this.client.on('message', async (msg) => {
+            try {
+            const actions = [
+                {
+                    //processo intermediário para identificar palavras-chave e acionar a função ping do Replyer
+                    keywords: ["fui roubado", "fui furtado", "fui assaltado", "roubo", "furto", "assalto", "roubado", "furtado", "assaltado", "roubada", "furtada", "assaltada"],
+                    action: () => this.replyer.seguro(msg.body.toLowerCase()),
+                }
+            ];
+
+            const message = msg.body.toLowerCase();
+
+            const rule = actions.find(({ keywords }) =>
+                keywords.some(word => message.includes(word))
+            );
+
+            if (!rule) return;
+
+            const response = await rule.action();
+            await msg.reply(response);}
+            catch (error) {
+                console.error("Erro ao processar a mensagem:", error);
+                this.initialize(); // Reinicia o bot em caso de erro
+            }
+        });
+
+
+        this.client.initialize();
+    }
+}
