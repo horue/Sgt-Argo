fa6a755d88788b24f2eff5e015796001c7d7bc07
Jorge <jorgejapao00@gmail.com>
2026-08-07 11:04:40 -0300

    keywords js file added

---
+++ presets/keywords.js
@@ -0,0 +1,28 @@
+const keywords = [
+    "câmera",
+    "camera",
+    "nikon",
+    "canon",
+    "fuji",
+    "fujifilm",
+    "panasonic",
+    "godox",
+    "flash",
+    "lente",
+    "seguro",
+    "roubo",
+    "furto",
+    "assalto",
+    "workshop",
+    "curso",
+    "lightroom",
+    "photoshop",
+    "evoto",
+    "aftershoot",
+    "fotografia",
+    "fotógrafo",
+    "freela",
+    "assistente"
+];
+
+export default keywords;
